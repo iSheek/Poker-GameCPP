1af844142a6df2b682c64e47fe1df02b72f4710a
48604 <igor.szyk@outlook.com>
2026-03-05 00:12:39 +0100

    add function for finding highest four of a kind, some optimisation and tweaks in HandScore, change return type for functions that find highest poker hand

--- include/HandEvaluator.h
+++ include/HandEvaluator.h
@@ -4,14 +4,16 @@
 #include "HandScore.h"
 #include <vector>
 #include <algorithm>
+#include <optional>
+#include <array>
 
 class HandEvaluator
 {
 private:
 
-	static bool checkForHighestStraightFlush(const std::vector<Card>& cardsToCheck)
+	static std::optional<std::vector<Card>> checkForHighestStraightFlush(const std::vector<Card>& cardsToCheck)
 	{
-		if (cardsToCheck.size() < 5) return false;
+		if (cardsToCheck.size() < 5) return std::nullopt;
 
 		int startingIndex = 1;
 		int endIndex = 5;
@@ -27,42 +29,120 @@ private:
 					break;
 				}
 			}
-			if (straightFlushIsPossible) return true;
+			if (straightFlushIsPossible)
+			{
+				return std::vector<Card>(cardsToCheck.begin() + startingIndex - 1, cardsToCheck.begin() + endIndex);
+			}
 			++startingIndex;
 			++endIndex;
 		}
 
 		// Check for royal flush (because ace in enum is 1)
-		if (cardsToCheck.front().rank == CardRank::KING && cardsToCheck[3].rank == CardRank::TEN && cardsToCheck.back().rank == CardRank::ACE) return true;
+		if (cardsToCheck.front().rank == CardRank::KING && cardsToCheck[3].rank == CardRank::TEN && cardsToCheck.back().rank == CardRank::ACE)
+		{
+			return std::vector<Card>{cardsToCheck[0], cardsToCheck[1], cardsToCheck[2], cardsToCheck[3], cardsToCheck.back()};
+		}
 
-		return false;
+		return std::nullopt;
 	}
 	
+	static std::optional<std::vector<Card>> checkForFourOfAKind(const std::vector<Card>& cardsToCheck)
+	{
+		std::array<int, 14> rankCounts = { 0 };
+		for (const auto& card : cardsToCheck)
+		{
+			rankCounts[(int)card.rank]++;
+		}
+
+		CardRank foundFourRank = CardRank::NONE;
+
+		bool foundHand = false;
+
+		for (int i = 1; i < rankCounts.size(); i++)
+		{
+			if (rankCounts[i] == 4)
+			{
+				foundFourRank = static_cast<CardRank>(i);
+				foundHand = true;
+				break;
+			}
+		}
+
+		if (!foundHand) return std::nullopt;
+
+		std::vector<Card> usedCards;
+		usedCards.reserve(5);
+
+		
+		bool foundKicker = false;
+		Card highestKicker;
+
+		// Additional 'if' to use Ace as kicker
+		if (rankCounts[1] > 0 && foundFourRank != CardRank::ACE)
+		{
+			for (const auto& card : cardsToCheck)
+			{
+				if ((int)card.rank == 1)
+				{
+					highestKicker = card;
+					foundKicker = true;
+					break;
+				}
+			}
+		}
+
+
+		// We put four of a kind cards into our vector to return
+		for (const auto& card : cardsToCheck)
+		{
+			if (card.rank == foundFourRank)
+			{
+				usedCards.push_back(card);
+			}
+			else if (!foundKicker)
+			{
+				highestKicker = card;
+				foundKicker = true;
+			}
+		}
+
+
+
+		usedCards.push_back(highestKicker);
+		return usedCards;
+
+	}
 
 
 public:
-	static PokerHand Evaluate(std::vector<Card> allCardsInHand)
+	static HandScore Evaluate(std::vector<Card> allCardsInHand)
 	{
 		std::sort(allCardsInHand.begin(), allCardsInHand.end(), [](const Card& a, const Card& b) {return a.rank > b.rank; });
 
 
-		std::vector<Card> suitBuckets[5];
+		std::array<std::vector<Card>, 5> suitBuckets;
 
 		for (const Card& card : allCardsInHand)
 		{
 			suitBuckets[static_cast<int>(card.suit)].push_back(card);
 		}
 
-		for (size_t i = 1; i < suitBuckets->size(); i++)
+		for (size_t i = 1; i < suitBuckets.size(); i++)
 		{
-			if (checkForHighestStraightFlush(suitBuckets[i]))
+			if (suitBuckets.size() < 5) continue;
+
+			if (auto sfCards = checkForHighestStraightFlush(suitBuckets[i]))
 			{
-				if (suitBuckets[i].front().rank == CardRank::KING && suitBuckets[i][3].rank == CardRank::TEN && suitBuckets[i].back().rank == CardRank::ACE) return PokerHand::ROYAL_FLUSH;
-				return PokerHand::STRAIGHT_FLUSH;
+				if (suitBuckets[i].front().rank == CardRank::KING)
+				{
+					return HandScore{ PokerHand::ROYAL_FLUSH, sfCards.value()};
+				}
+				return HandScore{ PokerHand::STRAIGHT_FLUSH, sfCards.value() };
 			}
 
-
 		}
+
+		checkForFourOfAKind(allCardsInHand);
 
 
 	}
--- include/HandScore.h
+++ include/HandScore.h
@@ -20,16 +20,32 @@ enum class PokerHand
 struct HandScore
 {
 	PokerHand hand;
-	std::vector<int> valuesInHand;
 	std::vector<Card> usedCardsInHand;
 
-	bool operator>(const HandScore& other)
+	int getCardStrength(CardRank rank) const
+	{
+		if (rank == CardRank::ACE) return 14;
+		return (int)rank;
+	}
+
+	bool operator>(const HandScore& other) const
 	{
 		if (hand != other.hand)
 		{
 			return hand > other.hand;
 		}
 
-		return valuesInHand > other.valuesInHand;
+		for (size_t i = 0; i < usedCardsInHand.size(); i++)
+		{
+			int myCardStrength = getCardStrength(usedCardsInHand[i].rank);
+			int otherCardStrength = getCardStrength(other.usedCardsInHand[i].rank);
+
+			if (myCardStrength != otherCardStrength)
+			{
+				return myCardStrength > otherCardStrength;
+			}
+		}
+
+		return false;
 	}
 };
