79faba18d5b4faab671d8bb63a62cb4e2b658216
48604 <igor.szyk@outlook.com>
2026-04-21 23:04:05 +0200

    finish implementing other methods in ConsoleMenuController.cpp (except run())

--- PokerClientCPP/src/ConsoleMenuController.cpp
+++ PokerClientCPP/src/ConsoleMenuController.cpp
@@ -7,8 +7,13 @@
 #include <limits>
 
 
-std::string_view wrongInputMessage{ "WRONG INPUT!" };
-std::string_view wrongNicknameMessage{ "WRONG NICKNAME, YOU CAN ONLY USE LETTERS!" };
+constexpr std::string_view wrongInputMessage{ "WRONG INPUT!" };
+constexpr std::string_view wrongNicknameMessage{ "WRONG NICKNAME, YOU CAN ONLY USE LETTERS!" };
+
+constexpr int maxBots{ 4 };
+
+constexpr std::string_view wrongBotCountMessage{ "WRONG BOT COUNT, YOU CAN PUT MAX 4 BOTS" };
+
 
 MenuChoice ConsoleMenuController::askForMenuChoice()
 {
@@ -62,23 +67,117 @@ std::string ConsoleMenuController::askForNickname()
 
 	}
 
+	return givenNickname;
 }
 
 std::string ConsoleMenuController::askForIP()
 {
+	
+	std::string givenIP{};
+	
+	bool showError{ false };
 
+	while (true)
+	{
+		this->view.showIPInput();
+		if (showError) this->view.showError(wrongInputMessage);
 
+		std::cin >> givenIP;
 
+		if (!std::cin)
+		{
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.clear();
+			showError = true;
+			continue;
+		}
+		else break;
+	}
+
+	return givenIP;
 }
 
 int ConsoleMenuController::askForPort()
 {
+
+	bool showError{ false };
+	int givenPort{};
+	while (true)
+	{
+		this->view.showPortInput();
+		if (showError) this->view.showError(wrongInputMessage);
+
+		std::cin >> givenPort;
+
+		if (!std::cin)
+		{
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.clear();
+			showError = true;
+			continue;
+		}
+		else break;
+
+	}
+
+	return givenPort;
+
 }
 
 int ConsoleMenuController::askForBotsCount()
 {
+
+	bool showError{ false };
+
+	int givenCount{};
+
+	while (true)
+	{
+		this->view.showBotCountInput();
+		if (showError) this->view.showError(wrongBotCountMessage);
+
+		std::cin >> givenCount;
+		if (!std::cin)
+		{
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.clear();
+			showError = true;
+			continue;
+		}
+		else if (0 > givenCount || maxBots < givenCount)
+		{
+			showError = true;
+			continue;
+		}
+		else break;
+	}
+
+	return givenCount;
+
+
 }
 
+
+// TODO: FINISH THIS
 void ConsoleMenuController::run()
 {
+	MenuChoice chosenOption{ askForMenuChoice() };
+
+	switch (chosenOption)
+	{
+	case MenuChoice::START_SINGLEPLAYER:
+		break;
+	case MenuChoice::START_MULTIPLAYER:
+		break;
+	case MenuChoice::JOIN_MULTIPLAYER:
+		break;
+	case MenuChoice::EXIT:
+		break;
+	case MenuChoice::numberofchoices:
+		break;
+	default:
+		break;
+	}
+
+
 }
